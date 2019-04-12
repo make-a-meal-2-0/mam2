30b75fdfe934fffe02f757a53fc04112ae940757
martyjapilado <martyjapilado@gmail.com>
2019-04-12 13:55:16 -1000

    Fixed up the Footer for issue 14

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px', backgroundColor: '#644021', color: 'white' };
+    const divStyle = { paddingTop: '15px', backgroundColor: '#644021', color: 'white',
+      height: '600px', width: '2000px' };
     return (
         <footer>
           <div style={divStyle} className="ui center aligned container">
             <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822
+              Make A Meal 2.0 <br />
+
           </div>
         </footer>
     );

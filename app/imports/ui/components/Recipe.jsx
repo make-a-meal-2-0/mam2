d3b619e2e347c82308e149a8171ebc883821d0e3
martyjapilado <martyjapilado@gmail.com>
2019-04-25 18:10:52 -1000

    Fixed merging issues in issue-28

--- app/imports/ui/components/Recipe.jsx
+++ app/imports/ui/components/Recipe.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Button, Card, Label } from 'semantic-ui-react';
-=======
-import { Card, Segment } from 'semantic-ui-react';
 import { Recipes, OwnerSchema } from '/imports/api/recipe/recipe';
->>>>>>> 5c2e05ad0c2b4fc8c4afd7ba51301201ef58d53f
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import AutoForm from 'uniforms-semantic/AutoForm';
@@ -13,7 +9,6 @@ import HiddenField from 'uniforms-semantic/HiddenField';
 import { Bert } from 'meteor/themeteorchef:bert';
 import { Meteor } from 'meteor/meteor';
 import ErrorsField from 'uniforms-semantic/ErrorsField';
-
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class Recipe extends React.Component {
@@ -53,36 +48,40 @@ class Recipe extends React.Component {
               {this.props.recipe.name}
             </Card.Header>
             <Card.Meta>
-              <Label color='red' ribbon>Preperation Time:<div className='detail'>{this.props.recipe.time}</div> </Label>
-            </Card.Meta>
-            <Label color='red' ribbon>Serving Size:<div className='detail'>{this.props.recipe.servingSize}</div>
-            </Label>
-            <Card.Meta>
-              <Label color='red' ribbon>Required Materials:<div className='detail'>{this.props.recipe.tool}</div>
+              <Label color='red' ribbon>Preperation Time:
+                <div className='detail'>{this.props.recipe.time}</div>
               </Label>
             </Card.Meta>
-             <Card.Content>
-                 <Label ribbon color='red'>
-                   Ingredients: <div className ='detail'>{this.props.ingredients.quantity}
-                   {this.props.ingredients.ingredient} {this.props.ingredients.measurement}</div>
-                 </Label>
-             </Card.Content>
+            <Label color='red' ribbon>Serving Size:
+              <div className='detail'>{this.props.recipe.servingSize}</div>
+            </Label>
+            <Card.Meta>
+              <Label color='red' ribbon>Required Materials:
+                <div className='detail'>{this.props.recipe.tool}</div>
+              </Label>
+            </Card.Meta>
+            <Card.Content>
+              <Label ribbon color='red'>
+                Ingredients: <div className='detail'>{this.props.ingredients.quantity}
+                {this.props.ingredients.ingredient} {this.props.ingredients.measurement}</div>
+              </Label>
+            </Card.Content>
             <Card.Description>
               {this.props.recipe.directions}
             </Card.Description>
             <AutoForm ref={(ref) => {
               this.formRef = ref;
             }} schema={OwnerSchema}
-                onSubmit={this.submit}>
-                <SubmitField value='Save'/>
+                      onSubmit={this.submit}>
+              <SubmitField value='Save'/>
               <ErrorsField/>
               <HiddenField name='owner' value='fakeuser@foo.com'/>
             </AutoForm>
           </Card.Content>
           <Card.Content extra>
-              <Button positive>
-                Save
-              </Button>
+            <Button positive>
+              Save
+            </Button>
           </Card.Content>
         </Card>
     );
@@ -94,7 +93,6 @@ Recipe.propTypes = {
   recipe: PropTypes.object.isRequired,
   ingredients: PropTypes.object.isRequired,
 };
-
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
 export default withRouter(Recipe);

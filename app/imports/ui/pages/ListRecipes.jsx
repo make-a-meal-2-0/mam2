4747f4f37fccf0848b96d7b4742ac2d0df80b5f0
Davin Takahashi <dvntaka@yahoo.com>
2019-05-06 22:31:43 -1000

    stuff

--- app/imports/ui/pages/ListRecipes.jsx
+++ app/imports/ui/pages/ListRecipes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Header, Card, Divider, Checkbox, Grid, Visibility, Ref } from 'semantic-ui-react';
+import { Container, Header, Card, Divider, Checkbox, Grid, Visibility, Ref, Segment} from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Recipes } from '/imports/api/recipe/recipe';
@@ -10,15 +10,25 @@ import Recipe from '../components/Recipe';
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListRecipes extends React.Component {
   state = {
-    calculations: {
-      visible: false,
-    },
-    showVegan: true,
-  }
+    vegan: false,
+    // vegetarian: false,
+    // nut: false,
+    // dairy: false,
+    // seafood: false,
+    // gforeman: false,
+    // microwave: false,
+    // toaster: false,
+    //
+    // {this.props.recipes.map((recipe) => <Recipe
+    //     key={recipe._id}
+    //     recipe={recipe}
+    //     ingredients={this.props.ingredients.filter(ingredient => (ingredient.name === recipe.name))}/>)}
+    //
+    //props.recipes.filter(recipe => (recipe.isVegan === true))
+  };
 
-  handleUpdate = (e, { calculations }) => this.setState({ calculations })
-
-  handleVegan = (e, { checked }) => this.setState({ showVegan: checked })
+  handleVegan = (e, { checked }) =>
+      this.setState({ vegan: checked })
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -28,41 +38,43 @@ class ListRecipes extends React.Component {
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
-    const { calculations, showVegan } = this.state;
+    const { vegan } = this.state;
     return (
-
-        <div className='ListBackground'>
+        <div className='Background'>
           <Container>
             <Header as='h2' textAlign='center' inverted>List Recipes</Header>
             <Grid>
               <Grid.Column width={2}>
-                <Checkbox toggle label='Vegan'/>
+                <Checkbox toggle label = 'Vegan' checked={vegan} onChange={this.handleVegan} />
               </Grid.Column>
               <Grid.Column width={2}>
-                <Checkbox toggle label='Vegetarian'/>
+                <Checkbox toggle label = 'Vegetarian' />
               </Grid.Column>
               <Grid.Column width={2}>
-                <Checkbox toggle label='Nut Free'/>
+                <Checkbox toggle label = 'Nut Free' />
               </Grid.Column>
               <Grid.Column width={2}>
-                <Checkbox toggle label='Dairy Free'/>
+                <Checkbox toggle label = 'Dairy Free' />
               </Grid.Column>
               <Grid.Column width={2}>
-                <Checkbox toggle label='Seafood Free'/>
+                <Checkbox toggle label = 'Seafood Free' />
               </Grid.Column>
               <Divider/>
             </Grid>
             <Card.Group content>
-              {this.props.recipes.map((recipe) => <Recipe
-                  key={recipe._id}
-                  recipe={recipe}
-                  ingredients={this.props.ingredients.filter(ingredient => (ingredient.name === recipe.name))}/>)}
-          </Card.Group>
+
+            </Card.Group>
           </Container>
         </div>
     );
   }
 }
+
+// {this.props.recipes.filter(recipe => (recipe.isVegan === true)).map((recipe) => <Recipe
+//     key={recipe._id}
+//     recipe={recipe}
+//     ingredients={this.props.ingredients.filter(ingredient => (ingredient.name === recipe.name))}/>)}}
+
 
 /** Require an array of Stuff documents in the props. */
 ListRecipes.propTypes = {
@@ -83,3 +95,4 @@ export default withTracker(() => {
     ready: (subscriptionR.ready() && subscriptionI.ready()),
   };
 })(ListRecipes);
+
